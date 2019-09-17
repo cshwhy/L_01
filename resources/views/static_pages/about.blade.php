7ff2a508707a578210468905bd5398f268a237c5
1138703080@qq.com <934574338@qq.com>
2019-09-17 20:58:32 +0800

    练习控制器

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    @section('title','关于')
+    <h1>关于页</h1>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+
+Route::get('/','StaticPagesController@home');
+Route::get('about','StaticPagesController@about');
+Route::get('help','StaticPagesController@help');
